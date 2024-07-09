ef891ee7660b3aa78d8d3106e21b8b833790a0b2
daffarafip24 <daffarafip24@gmail.com>
2024-07-09 19:59:19 +0700

    feat(my-tickets): ticket history responsive

--- src/components/modules/MyTicketModule/module-elements/TicketHistoryItem.tsx
+++ src/components/modules/MyTicketModule/module-elements/TicketHistoryItem.tsx
@@ -11,12 +11,14 @@ export const TicketHistoryItem: React.FC<TicketHistoryItemProps> = ({
   return (
     <Link
       href={`/my-tickets/${id}`}
-      className="drop-shadow-md p-6 rounded-2xl bg-white border border-[#F3F3F3] flex items-center gap-4 hover:bg-black/5 transition-all"
+      className="drop-shadow-md p-3 sm:p-6 rounded-2xl bg-white border border-[#F3F3F3] flex items-center gap-4 hover:bg-black/5 transition-all"
     >
       <div className="w-full flex flex-col gap-1">
-        <span className="text-sm text-[#606060]">ID: #{id}</span>
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <h3 className="text-[#606060]">{variant}</h3>
+        <span className="hidden sm:block text-sm text-[#606060]">
+          ID: #{id}
+        </span>
+        <h2 className="text-base sm:text-xl font-semibold">{title}</h2>
+        <h3 className="text-sm sm:text-base text-[#606060]">{variant}</h3>
       </div>
       <ChevronRight />
     </Link>

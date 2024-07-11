962f573d0931118a9634468089e93a037a18e4e5
isaui <isadestroyed17@gmail.com>
2024-07-11 13:38:13 +0700

    feat: full height card desktop

--- src/components/modules/VisitorModule/index.tsx
+++ src/components/modules/VisitorModule/index.tsx
@@ -6,9 +6,9 @@ const VisitorModule = () => {
     return <div className="flex flex-col w-screen min-h-screen bg-background">
         <Card className="w-full bg-white mx-auto   rounded-none max-w-4xl border-0 md:border 
         shadow-none">
-        <CardContent className="p-0">
+        <CardContent className="p-0 md:min-h-screen">
             <HeaderSection/>
-            <div className="flex flex-col w-full md:py-4">
+            <div className="flex flex-col w-full md:py-4 md:px-4">
             <ContentSection/>
             </div>
             

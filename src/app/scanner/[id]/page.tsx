097a867e466fe018697c68d7c172e74fcbf8f310
isaui <isadestroyed17@gmail.com>
2024-07-24 16:14:41 +0700

    fix: build

--- src/app/scanner/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import TicketDetailModule from '@/components/modules/TicketDetailModule';
-
-const page = ({ params }: { params: { id: string } }) => {
-  return <TicketDetailModule id={params.id} />;
-};
-
-export default page;

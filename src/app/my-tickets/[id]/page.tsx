193f814672d0800e63cbeb80250180120d01c55f
isaui <isadestroyed17@gmail.com>
2024-07-24 09:28:37 +0700

    chore: fix typescript file name

--- src/app/my-tickets/[id]/page.tsx
+++ src/app/my-tickets/[id]/page.tsx
@@ -2,6 +2,7 @@ import TicketDetailModule from "@/components/modules/TicketDetailModule";
 
 
 
+
 const page = ({ params }: { params: { id: string } }) => {
   return <TicketDetailModule id={params.id} />;
 };
